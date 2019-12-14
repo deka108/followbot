06e90978361586b594fa3b2c95e20331349b2396
deka108 <deka108@gmail.com>
2019-12-14 13:35:48 -0500

    Part 3

--- src/follower_bonus.py
+++ src/follower_bonus.py
@@ -16,7 +16,6 @@ class Follower:
     self.bridge = cv_bridge.CvBridge()
     cv2.namedWindow("window", 1)
     cv2.namedWindow("yellow_mask", 1)
-    cv2.namedWindow("red_mask", 1)
 
     self.image_sub = rospy.Subscriber('camera/rgb/image_raw', 
                                       Image, self.image_callback)
@@ -34,12 +33,7 @@ class Follower:
     lower_yellow = numpy.array([ 25,  40, 40])
     upper_yellow = numpy.array([35, 255, 250])
 
-    # red
-    lower_red = numpy.array([0, 40, 40])
-    upper_red = numpy.array([15, 255, 250])
-
     mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
-    mask_red = cv2.inRange(hsv, lower_red, upper_red)
     
     h, w, d = image.shape
     search_top = 3*h/4
@@ -49,36 +43,32 @@ class Follower:
     mask_yellow[0:search_top, 0:w] = 0
     mask_yellow[search_bot:h, 0:w] = 0
 
-    mask_red[0:search_top, 0:w] = 0
-    mask_red[search_bot:h, 0:w] = 0
-
     M_yellow = cv2.moments(mask_yellow)
-    M_red = cv2.moments(mask_red)
 
-    if M_red['m00'] > 0:
-      cnts_red = cv2.findContours(mask_red.copy(), cv2.RETR_EXTERNAL, 
-        cv2.CHAIN_APPROX_SIMPLE)[-2]
-      cx = int(M_red['m10']/M_red['m00'])
-      cy = int(M_red['m01']/M_red['m00'])
-      cv2.circle(image, (cx, cy), 20, (255,0,0), -1)
+    # if M_red['m00'] > 0:
+    #   cnts_red = cv2.findContours(mask_red.copy(), cv2.RETR_EXTERNAL, 
+    #     cv2.CHAIN_APPROX_SIMPLE)[-2]
+    #   cx = int(M_red['m10']/M_red['m00'])
+    #   cy = int(M_red['m01']/M_red['m00'])
+    #   cv2.circle(image, (cx, cy), 20, (255,0,0), -1)
       
-      for idx, cnt in enumerate(cnts_red):
-        # calculate number of sides
-        peri = cv2.arcLength(cnt, True) # finds the Contour Perimeter
-        approx = cv2.approxPolyDP(cnt, 0.05 * peri, True)
+    #   for idx, cnt in enumerate(cnts_red):
+    #     # calculate number of sides
+    #     peri = cv2.arcLength(cnt, True) # finds the Contour Perimeter
+    #     approx = cv2.approxPolyDP(cnt, 0.05 * peri, True)
 
-        if len(approx) > 3:
-          print("it might be a star...")
-          self.stop = True
+    #     if len(approx) > 3:
+    #       print("it might be a star...")
+    #       self.stop = True
       
-      # BEGIN CONTROL
-      err = cx - w/2
-      self.twist.linear.x = 0.8
-      self.twist.angular.z = -float(err) / 100
-      self.cmd_vel_pub.publish(self.twist)
-      # END CONTROL
+    #   # BEGIN CONTROL
+    #   err = cx - w/2
+    #   self.twist.linear.x = 0.8
+    #   self.twist.angular.z = -float(err) / 100
+    #   self.cmd_vel_pub.publish(self.twist)
+    #   # END CONTROL
     
-    elif M_yellow['m00'] > 0:
+    if M_yellow['m00'] > 0:
       cx = int(M_yellow['m10']/M_yellow['m00'])
       cy = int(M_yellow['m01']/M_yellow['m00'])
       cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
@@ -95,10 +85,9 @@ class Follower:
             
     cv2.imshow("window", image)
     cv2.imshow("yellow_mask", mask_yellow)
-    cv2.imshow("red_mask", mask_red)
     cv2.moveWindow("window", 1200, 50)
     cv2.moveWindow("yellow_mask", 500, 600)
-    cv2.moveWindow("red_mask", 1200, 600)
+    
     cv2.waitKey(3)
 
 if __name__ == "__main__":
